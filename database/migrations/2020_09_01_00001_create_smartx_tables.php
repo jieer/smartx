b684e873cb3a3285e4fc87c53a106b524ba3a34c
周立强 <zhouliqiang@smartwell.cn>
2022-03-04 16:18:00 +0800

    修复表名bug

--- database/migrations/2020_09_01_00001_create_smartx_tables.php
+++ database/migrations/2020_09_01_00001_create_smartx_tables.php
@@ -84,8 +84,8 @@ class CreateSmartXTables extends Migration
             $table->timestamps();
         });
         //smx_wx_app_mch
-        Schema::dropIfExists(config('smartx.database.wx_app_table'));
-        Schema::create(config('smartx.database.wx_app_table'), function (Blueprint $table) {
+        Schema::dropIfExists(config('smartx.database.wx_app_mch_table'));
+        Schema::create(config('smartx.database.wx_app_mch_table'), function (Blueprint $table) {
             $table->increments('id');
             $table->string('mch_key', 190)->nullable();
             $table->string('mch_id')->nullable();
